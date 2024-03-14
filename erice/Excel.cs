906eea8bcceea1adba701a32ead81130d977fe55
Harun29 <ibrahimovic.harun01@gmail.com>
2024-03-14 21:11:22 +0100

    Add project files.

---
+++ erice/Excel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace erice
+{
+    class Excel
+    {
+        string path = "";
+        _Excel.Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            if (ws.Cells[i, j].Value != null)
+            {
+                return Convert.ToString(ws.Cells[i, j].Value);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+}
---
+++ erice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace erice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Excel excel = new Excel(@"C:\Users\Korisnik\Desktop\c#\erice\erice\UKUPNI_BODOVI.xls", 1);
+                for(int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(excel.ReadCell(i, 2));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
